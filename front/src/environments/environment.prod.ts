fe1296b227e79b04a443ce63180143e93eae3b88
AlbaaDev <abi-boys@live.fr>
2026-02-11 13:09:04 +0100

    add azure prod environment

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    baseUrl : 'http://marketspace.us-east-1.elasticbeanstalk.com'
+    // baseUrl : 'http://marketspace.us-east-1.elasticbeanstalk.com'
+    baseUrl : 'spacemarket-back-hjapg7ekdub5fzeg.canadacentral-01.azurewebsites.net'
 }
