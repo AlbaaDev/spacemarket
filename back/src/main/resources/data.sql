2679e54ce2d79ce0a4a02484d0b6588d02ba90cc
AlbaaDev <abi-boys@live.fr>
2026-02-16 13:55:47 +0100

    remove front/dist from git and update env properties

--- back/src/main/resources/data.sql
+++ back/src/main/resources/data.sql
@@ -1,7 +1,17 @@
-INSERT INTO Company (name, country, city, adress) VALUES('Google', 'Suisse', 'google@gmail.com', 'Genève', "13 Rue Genève");
+-- Company (pas d'email, adresse complète)
+INSERT INTO Company (name, country, city, address) VALUES('Google', 'Suisse', 'Genève', '1 California Way');
+INSERT INTO Company (name, country, city, address) VALUES('Microsoft', 'USA', 'Seattle', '1 Microsoft Way');
+INSERT INTO Company (name, country, city, address) VALUES('Apple', 'USA', 'Cupertino', '1 Apple Park Way');
 
+-- Contacts
+INSERT INTO Contact (birth_date, country, email, first_name, last_name, phone, city, adress) 
+VALUES('2025-01-01', 'Suisse', 'test@live.fr', 'John1', 'Doe1', '0117684965', 'Genève', 'Rue de Genève');
 
-INSERT INTO Contact (birth_date, country, email, first_name, last_name, phone, city, adress) VALUES('2025-01-01', 'Suisse', 'test@live.fr', 'John1', 'Doe1', '0117684965', 'Genève', 'Rue de Genève');
-INSERT INTO Contact (birth_date, country, email, first_name, last_name, phone, city, adress) VALUES('2025-02-02', 'Suisse', 'test2@live.fr', 'John2', 'Doe2', '0217684965', 'Genève', 'Rue de Genève');
-INSERT INTO Contact (birth_date, country, email, first_name, last_name, phone, city, adress) VALUES('2025-03-03', 'France', 'test3@live.fr', 'John3', 'Doe3', '0317684965', 'Paris', 'Rue de France');
-INSERT INTO Contact (birth_date, country, email, first_name, last_name, phone, city, adress) VALUES('2025-04-04', 'USA', 'test4@live.fr', 'John4', 'Doe4', '0417684965', 'New Yrk', 'Rue de new york');
+INSERT INTO Contact (birth_date, country, email, first_name, last_name, phone, city, adress) 
+VALUES('2025-02-02', 'Suisse', 'test2@live.fr', 'John2', 'Doe2', '0217684965', 'Genève', 'Rue de Genève');
+
+INSERT INTO Contact (birth_date, country, email, first_name, last_name, phone, city, adress) 
+VALUES('2025-03-03', 'France', 'test3@live.fr', 'John3', 'Doe3', '0317684965', 'Paris', 'Rue de France');
+
+INSERT INTO Contact (birth_date, country, email, first_name, last_name, phone, city, adress) 
+VALUES('2025-04-04', 'USA', 'test4@live.fr', 'John4', 'Doe4', '0417684965', 'New York', 'Rue de New York');
