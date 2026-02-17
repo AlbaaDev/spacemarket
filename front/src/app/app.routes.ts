d37022aa9a54fd05d7ed5caadbc1241d48051ffd
AlbaaDev <abi-boys@live.fr>
2026-02-17 10:39:03 +0100

    remove automation in sidebar

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { AuthGuard } from '../guards/auth.guards';
 import { NoAuthGaurd } from '../guards/no-auth.guards';
-import { AutomationComponent } from '../pages/automation/automation.component';
 import { CalendrierComponent } from '../pages/calendrier/calendrier.component';
+import { CompaniesComponent } from '../pages/companies/companies.component';
 import { ContactComponent } from '../pages/contact/contact.component';
 import { DashboardComponent } from '../pages/dashboard/dashboard.component';
+import { DocumentsComponent } from '../pages/documents/documents.component';
 import { HomeComponent } from '../pages/home/home.component';
 import { LoginComponent } from '../pages/login/login.component';
 import { OpportunityComponent } from '../pages/opportunity/opportunity.component';
@@ -15,15 +16,13 @@ import { ReportingComponent } from '../pages/reporting/reporting.component';
 import { SettingsComponent } from '../pages/settings/settings.component';
 import { SignUpComponent } from '../pages/sign-up/sign-up.component';
 import { WorkflowComponent } from '../pages/workflow/workflow.component';
-import { DocumentsComponent } from '../pages/documents/documents.component';
-import { CompaniesComponent } from '../pages/companies/companies.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'app-home', component: HomeComponent },
   { path: 'app-login', component: LoginComponent, canActivate: [NoAuthGaurd] },
   { path: 'app-sign-up', component: SignUpComponent, canActivate: [NoAuthGaurd] },
-  { path: 'app-password-forgotten', component: PasswordForgottenComponent},
+  { path: 'app-password-forgotten', component: PasswordForgottenComponent },
   { path: 'app-logout', redirectTo: '/app-login' },
 
   { path: 'app-dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
@@ -33,10 +32,10 @@ export const routes: Routes = [
   { path: 'app-contact', component: ContactComponent, canActivate: [AuthGuard] },
   { path: 'app-opportunity', component: OpportunityComponent, canActivate: [AuthGuard] },
   { path: 'app-calendrier', component: CalendrierComponent, canActivate: [AuthGuard] },
-  { path: 'app-automation', component: AutomationComponent, canActivate: [AuthGuard] },
+  { path: 'app-workflow', component: WorkflowComponent, canActivate: [AuthGuard] },
   { path: 'app-reporting', component: ReportingComponent, canActivate: [AuthGuard] },
   { path: 'app-document', component: DocumentsComponent, canActivate: [AuthGuard] },
 
-  { path: '**', component: PageNotFoundComponent},
+  { path: '**', component: PageNotFoundComponent },
 
 ];
