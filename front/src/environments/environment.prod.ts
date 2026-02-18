d824a6d21e850cc9bfab3327a2f7258da99759e7
AlbaaDev <abi-boys@live.fr>
2026-02-18 14:09:10 +0100

    revert env prod url to azure

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    baseUrl : 'http://marketspace.us-east-1.elasticbeanstalk.com'
-    // baseUrl: 'https://spacemarket-back-hjapg7ekdub5fzeg.canadacentral-01.azurewebsites.net'
+    // baseUrl : 'http://marketspace.us-east-1.elasticbeanstalk.com'
+    baseUrl: 'https://spacemarket-back-hjapg7ekdub5fzeg.canadacentral-01.azurewebsites.net'
 }
